6dc9b9a7d79ec03aa8ccd9c53906fe1b7faa0663
Prithvipal Singh <prithvirathore99@gmail.com>
2020-10-10 13:17:40 +0530

    added file menu

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-const electron = require('electron'); 
+const electron = require('electron');
 const url = require('url');
 const path = require('path')
 
-const {app, BrowserWindow} = electron
+const { app, BrowserWindow, Menu } = electron
 
 let mainWindow;
 
 // Listen to app to be ready
-app.on('ready', function(){
+app.on('ready', function () {
     // Create new window
     mainWindow = new BrowserWindow({})
     // Load html into window
@@ -16,5 +16,31 @@ app.on('ready', function(){
         protocol: 'file:',
         slashes: true
     }));
+
+    // Build menu from template
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate)
+
+    // Insert menu
+    Menu.setApplicationMenu(mainMenu)
 })
 
+// Create menu template
+const mainMenuTemplate = [
+    {
+        label: 'File',
+        submenu: [
+            {
+                label: "Add Item"
+            },
+            {
+                label: "Clear Items"
+            },
+            {
+                label: "Quit",
+                click(){
+                    app.quit();
+                }
+            }
+        ]
+    }
+];
